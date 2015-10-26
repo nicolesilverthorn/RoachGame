28ff45f56f7dac3c4e6666ab19146fa23127f81e
nicolesilverthorn <nicolesilverthorn@hotmail.com>
2015-10-26 12:10:06 -0400

    updated game

--- roach game/Assets/Scripts/RoachController.cs
+++ roach game/Assets/Scripts/RoachController.cs
@@ -6,6 +6,9 @@ public class RoachController : MonoBehaviour
     public GameObject mRoach;
 
     private NavMeshAgent nav_;
+    private float maxDist_ = 30.0f;
+    private Vector3 dir_;
+    private Vector3 spawnPos_;
 
     public enum State
     {
@@ -20,29 +23,28 @@ public class RoachController : MonoBehaviour
     {
         Random.seed = System.DateTime.Now.Millisecond;
 
+        dir_ = Random.insideUnitSphere * maxDist_;
         nav_ = GetComponent<NavMeshAgent>();
         nav_.updateRotation = false;
 
-        for (int i = 0; i < 10; ++i) //test
-        {
-            //instantiate
-        }
+        float randX = Random.Range(-15.0f, 15.0f);
+        float randZ = Random.Range(-8.0f, 8.0f);
+        spawnPos_ = new Vector3(randX, 0.0f, randZ);
+
+        //for (int i = 0; i < numRoaches; i++)      <-- for loop crashes unity wtf
+        //{
+            Instantiate(mRoach, spawnPos_, Quaternion.identity);
+        //}
     }
 	
 	void Update ()
     {
         mCurrState = State.MOVING;
-        float randX = Random.Range(0.0f, Screen.width);
-        float randZ = Random.Range(0.0f, Screen.height);
-        nav_.SetDestination(new Vector3(randX, 0, randZ));
-        
-        //get vector to target (to - from)
-        Vector3 randRot = mRoach.transform.position - (new Vector3(randX, 0, randZ));
-        //get angle of vector to target
-        float angle = -Mathf.Atan2(randRot.z, randRot.x);
-        angle += Mathf.PI / 2.0f;
-        angle += Random.Range(-0.1f, 0.1f);
-        transform.rotation = Quaternion.Euler(90.0f, -90.0f, (angle / Mathf.PI) * 180.0f);
-        
+
+        dir_ += transform.position;
+        NavMeshHit hit;
+        NavMesh.SamplePosition(dir_, out hit, Random.Range(0f, maxDist_), 1);
+        Vector3 dest = hit.position;
+        nav_.SetDestination(dest);
     }
 }
